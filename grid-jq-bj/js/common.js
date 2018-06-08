b34724993fdebd89afbbf52a6b122c9ba351997f
zhaoxi521 <646151275@qq.com>
2018-06-08 19:52:49 +0800

    update

--- grid-jq-bj/js/common.js
+++ grid-jq-bj/js/common.js
@@ -1,11 +1,11 @@
-var dataUrl = '';
-//var dataUrl = 'http://121.69.136.97:9411';
+//var dataUrl = '';
+var dataUrl = 'http://121.69.136.97:9411';
 //var dataUrl = 'http://172.16.6.81:9411'
 //var dataUrl = 'http://172.16.200.20:19411'
-(function () {
+/*(function () {
     var winUrl = window.location.href.split(':18080')[0]
     dataUrl = winUrl + ':9411'
-})()
+})()*/
 
 //判断是否有用户登录，若登录，获取职能组信息，问题大类，区域
 isLogin() && getAndSetList()
